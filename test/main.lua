f1da921cf892647a16f62965c9ccca1edca2e6d8
EmekaNkurumeh <emekankurumeh@outlook.com>
2016-11-21 00:29:39 -0600

    ...

--- test/main.lua
+++ test/main.lua
@@ -7,15 +7,19 @@ image = {
   x = 64,
   y = 64,
   anfm = 0,
+  timer = 0
 }
-function play()
-  image.anfm = image.anfm + 1
+function play(dt)
+  image.timer = image.timer + dt
+  if image.timer > .1 then
+    image.anfm = image.anfm + 1
+    image.timer = 0
+  end
   if image.anfm > image.frames then
     image.anfm = 1
   end
 end
 screen = juno.Buffer.fromBlank(juno.graphics.getSize())
-animlayer = juno.Buffer.fromBlank(image.width,image.height)
 function juno.onLoad()
   juno.debug.setVisible(true)
   image.data = juno.Buffer.fromFile("zombie.png")
@@ -23,16 +27,14 @@ end
 function juno.onUpdate(dt)
   require("stalker").update(dt)
   tick.update(dt)
-  print(image.anfm)
-  -- play()
+  -- print(image.anfm)
+  play(dt)
 
 end
-tick.recur(function() play() end, .1)
 
 function juno.onDraw()
   screen:drawRect(0,0,256,256,unpack{1,1,1})
 
   screen:drawBuffer(image.data,image.x,image.y,{x = 0+(1*(image.anfm*image.width)), y = 0, w = 8, h = 13})
-  -- animlayer:copyPixels(image.data,0,0,{x = 0, y = 0, w = 8, h = 13})
   juno.graphics.copyPixels(screen,0,0,nil,4)
 end
